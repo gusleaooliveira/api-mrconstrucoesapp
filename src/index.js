782ce7beea2d3d7383eb1f90766b7f40ae2b3bd5
gusleaooliveira <gus.leaono@gmail.com>
2020-06-12 18:47:50 -0300

    alteração feita em:12/06/2020-18:47:50

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const knex = require('knex')({
-  client: mysql,
-  connection: {
-    host: 'ec2-34-202-88-122.compute-1.amazonaws.com',
-    user: 'df7fee437l9ba2',
-    password: 'ufttvdhihietlc',
-    database: 'id13817019_mrconstrucoes_api'
-  }
-});
+const { MongoClient } = require('mongodb');
+
+const url = "mongodb+srv://gusleaooliveira:65s-xtfuDTGH-Qj@cluster0-twwyw.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const dbName = "api_mrconstrucoes";
+const colName = "depoimento";
+
 
 
 app.use(bodyParser.json());
 
 app.get('/depoimentos', (req, res, next)=>{
-
+  async function run(){
+    try{
+       await client.connect();
+    }catch(err){
+      console.error('Erro: '+err.stack);
+    }finally {
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
 });
 
 app.get('/depoimentos/:id', (req, res, next) =>{
@@ -29,10 +35,28 @@ app.put('/depoimentos/:id', (req, res, next) =>{
 
 app.delete('/depoimentos/:id', (req, res, next) =>{
   let id = req.params.id;
+
 });
 
 app.post('/depoimentos', (req, res, next)=>{
   let requisicao = req.body;
+  const mongoClient = new MongoClient(url);
+  async function run(){
+    try{
+       await client.connect();
+       const db = client.db(dbName);
+       const col =  db.collection(colName);
+       const p = await col.insertOne(requisicao);
+       const resposta = await col.find();
+       res.send(resposta);
+    }catch(err){
+      console.error('Erro: '+err.stack);
+    }finally {
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
+  mongoClient.close();
 });
 
 
