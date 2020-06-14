278ce837908e74cf2552fe69f5b575091629a7ee
gusleaooliveira <gus.leaono@gmail.com>
2020-06-14 19:18:13 -0300

    alteração feita em:14/06/2020-19:18:13

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-const { MongoClient, ObjectId } = require('mongodb');
-const cors = require('cors');
+const { MongoClient, ObjectId } = require("mongodb");
+const cors = require("cors");
 
-const url = "mongodb+srv://gusleaooliveira:65s-xtfuDTGH-Qj@cluster0-twwyw.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const url =
+  "mongodb+srv://gusleaooliveira:65s-xtfuDTGH-Qj@cluster0-twwyw.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority";
 const dbName = "api_mrconstrucoes";
 const colName = "depoimento";
 
@@ -15,217 +16,212 @@ const client = new MongoClient(url);
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post('/imagens', (req, res, next)=>{
+app.post("/imagens", (req, res, next) => {
   let requisicao = req.body;
-  async function run(){
-    try{
-       await client.connect();
-       const db = client.db(dbName);
-       const col =  db.collection(colImage);
-       const p = await col.insertOne(requisicao);
-       const resposta = await col.findOne();
-       res.send(resposta);
-    }catch(err){
-      console.error('Erro: '+err.stack);
+  async function run() {
+    try {
+      await client.connect();
+      const db = client.db(dbName);
+      const col = db.collection(colImage);
+      const p = await col.insertOne(requisicao);
+      const resposta = await col.findOne();
+      res.send(resposta);
+    } catch (err) {
+      console.error("Erro: " + err.stack);
     }
   }
-  run().catch(console.dir); 
+  run().catch(console.dir);
 });
 
-
-app.put('/imagens', (req, res, next)=>{
+app.put("/imagens", (req, res, next) => {
   let requisicao = req.body;
-  async function run(){
-    try{
-       await client.connect();
-       const db = client.db(dbName);
-       const col =  db.collection(colImage);
-       const pre = await col.find({"_id": ObjectId(String(requisicao._id))}).toArray();
-       const resposta = await col.replaceOne(pre[0], {"tamanho": requisicao.tamanho, "imagem": requisicao.imagem});
-       console.log(resposta);
-       res.send(resposta);       
-    }catch(err){
-      console.error('Erro: '+err.stack);
+  async function run() {
+    try {
+      await client.connect();
+      const db = client.db(dbName);
+      const col = db.collection(colImage);
+      const pre = await col
+        .find({ _id: ObjectId(String(requisicao._id)) })
+        .toArray();
+      const resposta = await col.replaceOne(pre[0], {
+        tamanho: requisicao.tamanho,
+        imagem: requisicao.imagem,
+      });
+      console.log(resposta);
+      res.send(resposta);
+    } catch (err) {
+      console.error("Erro: " + err.stack);
     }
   }
-  run().catch(console.dir); 
+  run().catch(console.dir);
 });
 
-
-app.delete('/imagens/:id', (req, res, next)=>{
+app.delete("/imagens/:id", (req, res, next) => {
   let requisicao = req.params.id;
-  async function run(){
-    try{
-       await client.connect();
-       const db = client.db(dbName);
-       const col =  db.collection(colImage);
-       const pre = await col.find({"_id": ObjectId(String(requisicao))}).toArray();
-       const resposta = await col.deleteOne(pre[0]);
-       console.log(resposta);
-       res.send(resposta);       
-    }catch(err){
-      console.error('Erro: '+err.stack);
+  async function run() {
+    try {
+      await client.connect();
+      const db = client.db(dbName);
+      const col = db.collection(colImage);
+      const pre = await col
+        .find({ _id: ObjectId(String(requisicao)) })
+        .toArray();
+      const resposta = await col.deleteOne(pre[0]);
+      console.log(resposta);
+      res.send(resposta);
+    } catch (err) {
+      console.error("Erro: " + err.stack);
     }
   }
-  run().catch(console.dir); 
+  run().catch(console.dir);
 });
 
-app.get('/imagens/pequeno', (req, res, next) => {
-   async function run(){
-       try{
-           await client.connect();
-           const db = client.db(dbName);
-           const col = db.collection(colImage);
-           const resposta = await col.find({"tamanho": "pequeno"}).toArray();
-           console.log(resposta);
-           res.send(resposta);
-       }catch(err){
-           console.log('Erro: '+err.stack);
-       }
-   } 
-   run().catch(console.dir);
+app.get("/imagens/pequeno", (req, res, next) => {
+  async function run() {
+    try {
+      await client.connect();
+      const db = client.db(dbName);
+      const col = db.collection(colImage);
+      const resposta = await col.find({ tamanho: "pequeno" }).toArray();
+      console.log(resposta);
+      res.send(resposta);
+    } catch (err) {
+      console.log("Erro: " + err.stack);
+    }
+  }
+  run().catch(console.dir);
 });
 
-
-
-app.get('/imagens/medio', (req, res, next) => {
-   async function run(){
-       try{
-           await client.connect();
-           const db = client.db(dbName);
-           const col = db.collection(colImage);
-           const resposta = await col.find({"tamanho": "medio"}).toArray();
-           res.send(resposta);
-       }catch(err){
-           console.log('Erro: '+err.stack);
-       }
-   } 
-   run().catch(console.dir);
+app.get("/imagens/medio", (req, res, next) => {
+  async function run() {
+    try {
+      await client.connect();
+      const db = client.db(dbName);
+      const col = db.collection(colImage);
+      const resposta = await col.find({ tamanho: "medio" }).toArray();
+      res.send(resposta);
+    } catch (err) {
+      console.log("Erro: " + err.stack);
+    }
+  }
+  run().catch(console.dir);
 });
 
-app.get('/imagens/grande', (req, res, next) => {
-   async function run(){
-       try{
-           await client.connect();
-           const db = client.db(dbName);
-           const col = db.collection(colImage);
-           const resposta = await col.find({"tamanho": "grande"}).toArray();
-           res.send(resposta);
-       }catch(err){
-           console.log('Erro: '+err.stack);
-       }
-   } 
-   run().catch(console.dir);
+app.get("/imagens/grande", (req, res, next) => {
+  async function run() {
+    try {
+      await client.connect();
+      const db = client.db(dbName);
+      const col = db.collection(colImage);
+      const resposta = await col.find({ tamanho: "grande" }).toArray();
+      res.send(resposta);
+    } catch (err) {
+      console.log("Erro: " + err.stack);
+    }
+  }
+  run().catch(console.dir);
 });
-
 
 //--------------------------------------------------------------------------------------------------------------------------------
 
-app.get('/depoimentos', (req, res, next)=>{
-  async function run(){
-    try{
-       await client.connect();
-       const db = client.db(dbName);
-       const col =  db.collection(colName);
-       const resposta = await col.find().toArray();
-       res.send(resposta);
-    }catch(err){
-      console.error('Erro: '+err.stack);
+app.get("/depoimentos", (req, res, next) => {
+  async function run() {
+    try {
+      await client.connect();
+      const db = client.db(dbName);
+      const col = db.collection(colName);
+      const resposta = await col.find().toArray();
+      res.send(resposta);
+    } catch (err) {
+      console.error("Erro: " + err.stack);
     }
   }
   run().catch(console.dir);
 });
 
-
-
-
-
-app.get('/depoimentos/nomes', (req, res, next)=>{
-  async function run(){
-    try{
-       await client.connect();
-       const db = client.db(dbName);
-       const col =  db.collection(colName);
-       const resposta = await col.distinct("nome");
-       res.send(resposta);
-    }catch(err){
-      console.error('Erro: '+err.stack);
+app.get("/depoimentos/nomes", (req, res, next) => {
+  async function run() {
+    try {
+      await client.connect();
+      const db = client.db(dbName);
+      const col = db.collection(colName);
+      const resposta = await col.distinct("nome");
+      res.send(resposta);
+    } catch (err) {
+      console.error("Erro: " + err.stack);
     }
   }
   run().catch(console.dir);
 });
-app.get('/depoimentos/nomes/:nome', (req, res, next)=>{
+app.get("/depoimentos/nomes/:nome", (req, res, next) => {
   var nome = req.params.nome;
-  async function run(){
-    try{
-       await client.connect();
-       const db = client.db(dbName);
-       const col =  db.collection(colName);
-       const resposta = await col.find({"nome":  nome});
-       res.send(resposta);
-    }catch(err){
-      console.error('Erro: '+err.stack);
+  async function run() {
+    try {
+      await client.connect();
+      const db = client.db(dbName);
+      const col = db.collection(colName);
+      const resposta = await col.find({ nome: nome });
+      res.send(resposta);
+    } catch (err) {
+      console.error("Erro: " + err.stack);
     }
   }
   run().catch(console.dir);
 });
 
-app.get('/depoimentos/emails', (req, res, next)=>{
-  async function run(){
-    try{
-       await client.connect();
-       const db = client.db(dbName);
-       const col =  db.collection(colName);
-       const resposta = await col.distinct("email");
-       res.send(resposta);
-    }catch(err){
-      console.error('Erro: '+err.stack);
+app.get("/depoimentos/emails", (req, res, next) => {
+  async function run() {
+    try {
+      await client.connect();
+      const db = client.db(dbName);
+      const col = db.collection(colName);
+      const resposta = await col.distinct("email");
+      res.send(resposta);
+    } catch (err) {
+      console.error("Erro: " + err.stack);
     }
   }
   run().catch(console.dir);
 });
-app.get('/depoimentos/emails/:email', (req, res, next)=>{
+app.get("/depoimentos/emails/:email", (req, res, next) => {
   var email = req.params.email;
-  async function run(){
-    try{
-       await client.connect();
-       const db = client.db(dbName);
-       const col =  db.collection(colName);
-       const resposta = await col.find({"email": email});
-       res.send(resposta);
-    }catch(err){
-      console.error('Erro: '+err.stack);
+  async function run() {
+    try {
+      await client.connect();
+      const db = client.db(dbName);
+      const col = db.collection(colName);
+      const resposta = await col.find({ email: email });
+      res.send(resposta);
+    } catch (err) {
+      console.error("Erro: " + err.stack);
     }
   }
   run().catch(console.dir);
 });
 
-
-app.put('/depoimentos/:id', (req, res, next) =>{
+app.put("/depoimentos/:id", (req, res, next) => {
   let id = req.params.id;
 });
 
-app.delete('/depoimentos/:id', (req, res, next) =>{
+app.delete("/depoimentos/:id", (req, res, next) => {
   let id = req.params.id;
-
 });
 
-app.post('/depoimentos', (req, res, next)=>{
+app.post("/depoimentos", (req, res, next) => {
   let requisicao = req.body;
-  async function run(){
-    try{
-       await client.connect();
-       const db = client.db(dbName);
-       const col =  db.collection(colName);
-       const p = await col.insertOne(requisicao);
-       const resposta = await col.findOne();
-       res.send(resposta);
-    }catch(err){
-      console.error('Erro: '+err.stack);
+  async function run() {
+    try {
+      await client.connect();
+      const db = client.db(dbName);
+      const col = db.collection(colName);
+      const p = await col.insertOne(requisicao);
+      const resposta = await col.findOne();
+      res.send(resposta);
+    } catch (err) {
+      console.error("Erro: " + err.stack);
     }
   }
   run().catch(console.dir);
 });
-
 
 app.listen(process.env.PORT || 3000);
